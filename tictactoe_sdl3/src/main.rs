2c39d6e080a512bcd27ed194cf1314d877afeb8c
ZeissS <zeisss@moinz.de>
2026-08-08 23:22:20 +0200

    Unwrap cleanup

--- tictactoe_sdl3/src/main.rs
+++ tictactoe_sdl3/src/main.rs
@@ -1,7 +1,7 @@
 use tictactoe_ratatui::tictactoe::{Cell, GameState, Outcome, PlaceError, Player};
 
 use sdl3::event::Event;
-use sdl3::keyboard::{Keycode, Scancode, Mod};
+use sdl3::keyboard::{Keycode, Mod, Scancode};
 use sdl3::pixels::Color;
 use sdl3::render::{Canvas, FPoint};
 use sdl3::video::Window;
@@ -27,7 +27,7 @@ struct App {
 }
 
 impl App {
-    fn render(&mut self) {
+    fn render(&mut self) -> Result<(), sdl3::Error> {
         // Background color
         self.canvas.set_draw_color(LIGHT_GREY);
         self.canvas.clear();
@@ -38,14 +38,12 @@ impl App {
         // 200x200 per cell - 5 on each side
         for x in 0..3 {
             for y in 0..3 {
-                self.canvas
-                    .fill_rect(Rect::new(
-                        x * CELL_SIZE + 5,
-                        y * CELL_SIZE + 5,
-                        (CELL_SIZE - 10) as u32,
-                        (CELL_SIZE - 10) as u32,
-                    ))
-                    .unwrap();
+                self.canvas.fill_rect(Rect::new(
+                    x * CELL_SIZE + 5,
+                    y * CELL_SIZE + 5,
+                    (CELL_SIZE - 10) as u32,
+                    (CELL_SIZE - 10) as u32,
+                ))?;
             }
         }
 
@@ -64,7 +62,7 @@ impl App {
                     Cell::PlayerOccupied(Player::Cross) => draw_cross_cell(&mut self.canvas, x, y),
                     Cell::PlayerOccupied(Player::Naught) => {
                         draw_naught_cell(&mut self.canvas, x, y)
-                    } // TODO
+                    }
                     Cell::Empty => {
                         let label = match (x, y) {
                             (0, 0) => "Q",
@@ -82,8 +80,7 @@ impl App {
                         };
                         self.canvas.draw_debug_text(label, middle)
                     }
-                }
-                .unwrap();
+                }?;
             }
         }
 
@@ -96,38 +93,37 @@ impl App {
                     Outcome::CrossWins => "Cross wins!",
                     Outcome::Draw => "Draw - no winner!",
                 };
-                self.canvas.draw_debug_text(message, (AREA_X, 10.0)).unwrap();
-            },
+                self.canvas.draw_debug_text(message, (AREA_X, 10.0))?;
+            }
             None => {
                 self.canvas.set_draw_color(BLACK);
+                self.canvas
+                    .draw_debug_text("Active Player:".into(), (AREA_X, 10.0))?;
 
                 self.canvas
-                    .draw_debug_text("Active Player:".into(), (AREA_X, 10.0))
-                    .unwrap();
-
-                self.canvas.draw_debug_text(
-                    &self.board.active_player.to_string(),
-                    (AREA_X, 20.0),
-                ).unwrap();
+                    .draw_debug_text(&self.board.active_player.to_string(), (AREA_X, 20.0))?;
 
                 if let Err(error) = self.last_placement {
                     self.canvas.set_draw_color(RED);
-                    self.canvas.draw_debug_text(format!("Error: {:?}", error).as_str(), (AREA_X, 30.0)).unwrap();
+                    self.canvas
+                        .draw_debug_text(format!("Error: {:?}", error).as_str(), (AREA_X, 30.0))?;
                 };
             }
         };
 
         // Help text bottom right
         self.canvas.set_draw_color(BLACK);
-        self.canvas.draw_debug_text("Shift+R: Reset board", (AREA_X, 550.0)).unwrap();
-        self.canvas.draw_debug_text("Esc: Quit game", (AREA_X, 570.0)).unwrap();
+        self.canvas
+            .draw_debug_text("Shift+R: Reset board", (AREA_X, 550.0))?;
+        self.canvas
+            .draw_debug_text("Esc: Quit game", (AREA_X, 570.0))?;
 
         self.canvas.present();
+        Ok(())
     }
 
     fn process_inputs(&mut self, event_pump: &mut sdl3::EventPump) {
         for event in event_pump.poll_iter() {
-
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
@@ -136,22 +132,53 @@ impl App {
                 } => self.quit = true,
 
                 // Reset
-                Event::KeyDown {scancode: Some(Scancode::R), keymod, .. } if keymod.contains(Mod::LSHIFTMOD) => self.reset_board(),
+                Event::KeyDown {
+                    scancode: Some(Scancode::R),
+                    keymod,
+                    ..
+                } if keymod.contains(Mod::LSHIFTMOD) => self.reset_board(),
 
                 // Cell inputs
                 // Scancode since we don't care about what is printed on the keycap, we want the physical button layout
                 // See https://wiki.libsdl.org/SDL3/BestKeyboardPractices#the-101-button-joystick
-                Event::KeyDown {scancode: Some(Scancode::Q), .. } => self.last_placement = self.board.place(0,0),
-                Event::KeyDown {scancode: Some(Scancode::W), .. } => self.last_placement = self.board.place(1,0),
-                Event::KeyDown {scancode: Some(Scancode::E), .. } => self.last_placement = self.board.place(2,0),
+                Event::KeyDown {
+                    scancode: Some(Scancode::Q),
+                    ..
+                } => self.last_placement = self.board.place(0, 0),
+                Event::KeyDown {
+                    scancode: Some(Scancode::W),
+                    ..
+                } => self.last_placement = self.board.place(1, 0),
+                Event::KeyDown {
+                    scancode: Some(Scancode::E),
+                    ..
+                } => self.last_placement = self.board.place(2, 0),
 
-                Event::KeyDown {scancode: Some(Scancode::A), .. } => self.last_placement = self.board.place(0,1),
-                Event::KeyDown {scancode: Some(Scancode::S), .. } => self.last_placement = self.board.place(1,1),
-                Event::KeyDown {scancode: Some(Scancode::D), .. } => self.last_placement = self.board.place(2,1),
+                Event::KeyDown {
+                    scancode: Some(Scancode::A),
+                    ..
+                } => self.last_placement = self.board.place(0, 1),
+                Event::KeyDown {
+                    scancode: Some(Scancode::S),
+                    ..
+                } => self.last_placement = self.board.place(1, 1),
+                Event::KeyDown {
+                    scancode: Some(Scancode::D),
+                    ..
+                } => self.last_placement = self.board.place(2, 1),
 
-                Event::KeyDown {scancode: Some(Scancode::Z), .. } => self.last_placement = self.board.place(0,2),
-                Event::KeyDown {scancode: Some(Scancode::X), .. } => self.last_placement = self.board.place(1,2),
-                Event::KeyDown {scancode: Some(Scancode::C), .. } => self.last_placement = self.board.place(2,2),
+                Event::KeyDown {
+                    scancode: Some(Scancode::Z),
+                    ..
+                } => self.last_placement = self.board.place(0, 2),
+                Event::KeyDown {
+                    scancode: Some(Scancode::X),
+                    ..
+                } => self.last_placement = self.board.place(1, 2),
+                Event::KeyDown {
+                    scancode: Some(Scancode::C),
+                    ..
+                } => self.last_placement = self.board.place(2, 2),
 
                 _ => {}
             }
@@ -217,7 +244,7 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
     while !app.quit {
         app.process_inputs(&mut event_pump);
-        app.render();
+        app.render().unwrap();
         // The rest of the game loop goes here...
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
